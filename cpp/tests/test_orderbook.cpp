34688841905a8956da5c696fcf50dc47e2f043f3
rwh9609 <rwh9609@gmail.com>
2025-08-09 20:55:53 -0700

    fix cpp test

--- cpp/tests/test_orderbook.cpp
+++ cpp/tests/test_orderbook.cpp
@@ -17,12 +17,16 @@ TEST(OrderBookTest, AddAndQueryOrder) {
 
 TEST(OrderBookTest, CancelOrder) {
     int buy_id = ob_add_order(101.0, 10, 1);
+    ASSERT_GT(buy_id, 0);
     int result = ob_cancel_order(buy_id);
     EXPECT_EQ(result, 1);
     double price = 0;
     int qty = 0;
     int oid = 0;
     ob_get_top_of_book(1, &price, &qty, &oid);
+    // After cancelling the order, the top of the book should either be 0
+    // Or the initial order
+    // This test needs to be improved to handle the case where the cancelled order was the top of the book
     EXPECT_EQ(oid, 1);
     EXPECT_DOUBLE_EQ(price, 100);
     EXPECT_EQ(qty, 5);
