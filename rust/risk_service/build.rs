c099104bab96e3a28f60a5f33523e782e027879f
rwh9609 <rwh9609@gmail.com>
2025-08-15 17:14:51 -0700

    Add protoc to the risk build image (Dockerfile.risk) and copy protos into build context. Adjust build.rs to use the new proto path.

--- rust/risk_service/build.rs
+++ rust/risk_service/build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // In container the Dockerfile copies the root protos directory to ./protos
+    // so we reference it relative to the crate root.
     tonic_build::configure()
-        .compile(&["../../protos/trading_api.proto"], &["../../protos"])?;
+        .compile(&["protos/trading_api.proto"], &["protos"])?;
     Ok(())
 }
